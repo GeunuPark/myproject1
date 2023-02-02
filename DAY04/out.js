876d33df9f6a3152c343ac4b9ec4d6fb7cd3bfcd
GeunuPark <124110673+GeunuPark@users.noreply.github.com>
2023-02-02 17:50:58 +0900

    DAY 04

---
+++ DAY04/out.js
@@ -0,0 +1,21 @@
+function 빨래하다() {
+    console.log("1. 먼지를 턴다.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 막 비빈다.")
+    console.log("5. 물에 헹군다.")
+    console.log("6. 탈수한다.")
+    console.log("7. 건조한다.")
+}
+function 청소하기() {
+    console.log("1. 눈에 보이는 걸 먼저 줍자.")
+    console.log("2. 창문을 연다.")
+    console.log("3. 환기를 시킨다.")
+    console.log("4. 쓸모없는 것들을 버린다.")
+    console.log("5. 먼지를 털어낸다.")
+    console.log("6. 구석구석을 젖은 걸레로 닦는다.")
+    console.log("7. 청소기로 먼지를 치워낸다.")
+    console.log("8. 창문을 닫고 환기를 끝낸다.")
+    console.log("9. 물건을 제자리로 정리정돈한다.")
+    console.log("10. 배출된 쓰레기를 분리수거하여 버린다.")            
+}
---
+++ DAY04/out2.js
@@ -0,0 +1,18 @@
+function 여행하다() {
+    console.log("1. 어디를 갈지 정한다.")
+    console.log("2. 언제 갈지를 정한다.")
+    console.log("3. 교통편을 찾아본다.")
+    console.log("4. 숙소를 정한다.")
+    console.log("5. 주변 맛집을 탐색한다.")
+    console.log("6. 여행 자금을 확인한다.")
+    console.log("7. 예약을 한다.")
+}
+function 쉬다() {
+    console.log("1. 게임을 한다.")
+    console.log("2. 친구를 만난다.")
+    console.log("3. 여행을 간다.")
+    console.log("4. 술을 마신다.")
+    console.log("5. 대화를 나눈다.")
+    console.log("6. 음악을 듣는다.")
+    console.log("7. 영화를 감상한다.")
+}
